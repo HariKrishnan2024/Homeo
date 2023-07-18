1194813b221e11a2dc23f488bee6b779226ce165
Harikrish@123 <harikrishnanb2022@gmail.com>
2023-07-18 22:26:48 +0530

    fixes google indexing issues

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -14,7 +14,7 @@ export default function handler(req, res) {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
     <url>
-      <loc>${baseUrl}/Home</loc>
+      <loc>${baseUrl}/home</loc>
     </url>
      <url>
       <loc>${baseUrl}/index.js</loc>
